b0147d27675cb2988331a14e43a5883d7aa162fc
Charles Dyason <k4gcybex@gmail.com>
2023-03-15 10:46:52 +0200

    remove logging

--- reservations-backend-server/src/main/java/com/digicert/hotelreservations/controllers/HotelSiteController.java
+++ reservations-backend-server/src/main/java/com/digicert/hotelreservations/controllers/HotelSiteController.java
@@ -4,7 +4,6 @@ import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,7 +15,6 @@ import java.util.List;
 //@RestController
 @Controller
 @RequestMapping("sites")
-//@Slf4j
 //@Path("/sites")
 public class HotelSiteController {
 
