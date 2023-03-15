09a9d2793cb1015c8f2095e8b60235cf5090c9ae
Charles Dyason <k4gcybex@gmail.com>
2023-03-15 10:41:07 +0200

    w

--- reservations-backend-server/src/main/java/com/digicert/hotelreservations/config/JerseyConfiguration.java
+++ reservations-backend-server/src/main/java/com/digicert/hotelreservations/config/JerseyConfiguration.java
@@ -1,12 +1,13 @@
 package com.digicert.hotelreservations.config;
 
 import com.digicert.hotelreservations.controllers.HotelSiteController;
+import jakarta.ws.rs.ApplicationPath;
 import org.glassfish.jersey.server.ResourceConfig;
 import org.springframework.stereotype.Component;
 
 @Component
-public class JerseyConfiguration
-        extends ResourceConfig {
+@ApplicationPath("/api")
+public class JerseyConfiguration extends ResourceConfig {
     public JerseyConfiguration() {
         register(HotelSiteController.class);
     }
--- reservations-backend-server/src/main/java/com/digicert/hotelreservations/controllers/HotelSiteController.java
+++ reservations-backend-server/src/main/java/com/digicert/hotelreservations/controllers/HotelSiteController.java
@@ -1,27 +1,51 @@
 package com.digicert.hotelreservations.controllers;
 
-import com.digicert.hotelreservations.models.HotelSite;
-import com.digicert.hotelreservations.repository.HotelSiteRepository;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@RestController
-@RequestMapping("/")
+//@RestController
+@Controller
+@RequestMapping("sites")
+//@Slf4j
+//@Path("/sites")
 public class HotelSiteController {
 
-    // TODO migrate away from DI, see https://www.springcloud.io/post/2022-08/spring-field-injection/#gsc.tab=0
-    @Autowired
-    private HotelSiteRepository hotelSiteRepository;
+//    @GET
+//    # @Path("/hello")
+//    @Produces({ MediaType.APPLICATION_JSON})
+//    public @ResponseBody String greeting() {
+//        return "Hello, World";
+//    }
 
-    @GetMapping("/all")
-    @Produces({ MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML })
-    public List<HotelSite> getAllHotelSites() {
-        return (List<HotelSite>) hotelSiteRepository.findAll();
+//    @RequestMapping("/greeting")
+//    public @ResponseBody String greeting() {
+//        return "Hello, World";
+//    }
+
+//    @RequestMapping("/")
+    @GetMapping(path = "/hello", produces = "application/json")
+//    @Produces({ MediaType.APPLICATION_JSON})
+    public @ResponseBody String hello() {
+        return "Hello";
     }
+
+//     TODO migrate away from DI, see https://www.springcloud.io/post/2022-08/spring-field-injection/#gsc.tab=0
+//    @Autowired
+//    private HotelSiteRepository hotelSiteRepository;
+
+//    @GetMapping("/all")
+//    @Produces({ MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML })
+//    public List<HotelSite> getAllHotelSites() {
+//        return (List<HotelSite>) hotelSiteRepository.findAll();
+//    }
 }
